049098b66def200a8163206a998ad83de9e5a01b
Vijay Poduval <vsprulz@gmail.com>
2014-05-03 00:11:03 -0400

    SineWave
    Added new sine wave script

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ The following are the names and a brief decription of files:
 2) BoundingFrame.cs - This script is used to draw a 2D frame around an object using it's bounding box.
 3) CoolDown.cs - This function draws a cool down timer in the UI by interpolating the alpha gradient of the texture of the object.
 4) GameTimer.cs - This is a basic timer for a game.
+5) SineWave.cs - This is script to generate a sine wave motion in x,y plane. (Snake like animation)
---
+++ SineWave.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SineWave : MonoBehaviour {
+
+
+	// public members
+	public float amplitude = 10.0f;
+	public float frequency = 10.0f;
+	public float delay = 0.3f;
+	public float start_position = -5.0f; 
+	public float duration = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(sine_wave());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	IEnumerator sine_wave()
+	{
+
+
+		float curr_time = start_position;
+		float x = start_position;
+		float y = 0.0f;
+
+		while( curr_time < duration	)
+		{
+			y = amplitude * Mathf.Sin(curr_time);
+			// Un-Comment this section inorder to make it translate in x -direction
+			//x = frequency * curr_time;
+			transform.localPosition = new Vector3(x,y,0.0f);
+			curr_time += frequency * delay;
+			yield return new WaitForSeconds(delay);
+		}
+	}
+}
